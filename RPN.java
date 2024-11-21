99738afd96f915bc917aa6100da45e4772e43b8a
Smizzyy <schmidt3798@yahoo.de>
2024-11-21 14:00:53 +0100

    new

---
+++ RPN.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class RPN {
+    Stack<Float> calcStack;
+    char[] tokens;
+
+    RPN(String calcTokens) {
+        this.tokens = calcTokens.toCharArray();
+        this.calcStack = new Stack<>();
+    }
+
+    float evaluateRPN(String s) {
+        float result = 0;
+        
+
+
+        return result;
+    }
+}
