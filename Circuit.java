5ec10428e997b9f1dcffe1b07d8d48475a3d9e33
Smizzyy <schmidt3798@yahoo.de>
2024-12-13 16:55:44 +0100

    Map Gate

--- Circuit.java
+++ Circuit.java
@@ -2,21 +2,17 @@ import java.awt.Point;
 import java.util.HashMap;
 import java.util.Map;
 
-class Circuit {
-    private Turtle turtle1;
+class Circuit<T> {
     private String name;
-    private Map<Point, Gate> gates;
-    private Map<Point, Wire> wires;
-    private Map<Point, Input> inputs;
-    int width = 1700;
-    int height = 1700;
+    private Map<Point, T> components;
+    private Turtle turtle1;
+    private int width = 1700;
+    private int height = 1700;
     
-
+    // Konstruktor
     Circuit(String name) {
         this.turtle1 = new Turtle(this.width, this.height);
-        this.gates = new HashMap<>();
-        this.wires = new HashMap<>();
-        this.inputs = new HashMap<>();
+        this.components = new HashMap<>();
         this.name = name;
     }
 
@@ -28,67 +24,101 @@ class Circuit {
             turtle1.penUp();
         }
     }
+
+    void drawPointHorizontal() {
+        for (int i = 0; i < 4; i++) {
+            turtle1.penDown();
+            if (i % 2 == 0) turtle1.forward(100).right(90);
+            else turtle1.forward(50).right(90);
+        }
+    }
+
+    void drawPointVertical() {
+        for (int i = 0; i < 4; i++) {
+            turtle1.penDown();
+            if (i % 2 == 0) turtle1.forward(50).right(90);
+            else turtle1.forward(100).right(90);
+        }
+    }
     
     // Schaltungfeld zeichnen
-    void drawCircuitField(int rows, int cols) {
-        int x = 50;
-        int y = 50;
-        turtle1.moveTo(75, 40).left(90).penUp().forward(10).text("Only inputs for the first column").right(90);
+    void drawCircuitField(int cols, int rows) {
+        assert (rows > 0 && cols > 0); 
+        int x = 50, y = 50;
+        // Anzahl Reihen und Spalten zeichnen
+        turtle1.moveTo(x, 0);
+        for (int i = 0; i < rows; i++) { 
+            turtle1.color(255, 0, 0).penUp().left(90).backward(35).text("     " + (i + 1), null, 25, null).forward(35).right(90).color(0, 0, 0);
+            drawPointHorizontal();
+            turtle1.penUp().forward(100);
+        }
+        turtle1.moveTo(0, y);
+        for (int i = 0; i < cols; i++) {
+            turtle1.color(255, 0, 0).penUp().forward(10).left(90).backward(60).text("" + (i + 1), null, 25, null).forward(60).right(90).backward(10).color(0, 0, 0);
+            drawPointVertical();
+            turtle1.penUp().right(90).forward(100).left(90);
+        }
+        // Feld zeichnen
         turtle1.moveTo(x, y);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                turtle1.penDown();
                 drawSquare();
-                turtle1.penUp().forward(15).right(90).forward(10).left(180).text((i + 1) + ", " + (j + 1)).forward(10).left(90).forward(15).right(180); // jede Zelle mit der Koordinate markieren
                 turtle1.penUp().forward(100); // zur Position der nächsten Zelle bewegen
             }
             turtle1.moveTo(x, y += 100);
         }
-        turtle1.penUp();
     }
 
     // Komponente hinzufügen
-    void addGate(int row, int col, Gate gate) {
-        
+    void addComponent(int col, int row, T component) {
+        Point position = new Point(col, row);
+        if (components.containsKey(position)) throw new IllegalArgumentException("An dieser Position existiert bereits eine Komponente!");
+        else components.put(position, component);
+        System.out.println(component + " an Position " + col + ", " + row + " hinzugefuegt.");
     }
 
-    void addWire(int row, int col, Wire wire) {
-        
-    }
-
-    void addInput(int row, int col, Input input) {
-        
+    Point getPosition(T component) {
+        for (Map.Entry<Point, T> entry : components.entrySet()) { // iteriert über alle Schlüssel-Wert-Paare
+            if (entry.getValue().equals(component)) return entry.getKey(); // Position der Komponente gefunden
+        }
+        return null; // nicht gefunden
     }
 }
-
-
 
 // Gatter mit verschiedener Logik
 class Gate {
     private String type;
+    private String name;
     private int input1;
     private int input2;
     private int output;
 
-    Gate(String type) {
+    // Konstruktor
+    Gate(String type, String name) {
         this.type = type.toUpperCase();
+        this.name = name;
+        this.output = inputToOutput();
     }
 
-   
+    // wandelt den input je nach Logik in output um
+    int inputToOutput() {
+        output = switch (type) {
+            case "AND" -> input1 & input2;
+            case "OR" -> input1 | input2;
+            case "XOR" -> input1 ^ input2;
+            case "NAND" -> ~(input1 & input2);
+            case "NOR" -> ~(input1 | input2);
+            case "XNOR" -> ~(input1 ^ input2);
+            case "NOT" -> ~input1;
+            default -> throw new IllegalArgumentException("Unbekannter Gate-Typ: " + type);
+        };
+        return output;
+    }
 
-
-/*
-    switch (this.type = type.toUpperCase()) {
-        case "AND" -> input1 & input2;
-        case "OR" -> input1 | input2;
-        case "XOR" -> input1 ^ input2;
-        case "NAND" -> ~(input1 & input2);
-        case "NOR" -> ~(input1 | input2);
-        case "XNOR" -> ~(input1 ^ input2);
-        case "NOT" -> ~input1;
-        default -> throw new IllegalArgumentException("Unbekannter Gate-Typ: " + type);
-    };
-    */
+    @Override
+    public String toString() {
+        return "Gate type: " + type;
+    }
 }
 
 class Wire {
@@ -98,3 +128,6 @@ class Wire {
 class Input {
 
 }
+
+// Circuit<Object> c1 = new Circuit<>("Circ 1");
+// c1.drawCircuitField(15, 15);
